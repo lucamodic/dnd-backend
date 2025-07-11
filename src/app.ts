5c39319af76d6d566c751f29eeb62ebd3a307902
Luca Modic <lucanmodic@gmail.com>
2025-07-10 21:46:06 -0300

    base project working

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import { authRouter } from "./modules/index"
+import cors from "cors"
 
 dotenv.config()
 
-const port = process.env.PORT || 3000
-
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use("/auth", authRouter)
 
 export default app
